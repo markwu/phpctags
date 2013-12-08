4f58e6a4ec3e285b4be0622896386913d8a837ba
Mark Wu <wuhsinyee@hotmail.com>
2013-12-09 02:03:47 +0800

    Fisrt commit of porting phptags CLI to Symfony Console

---
+++ src/PHPCtags/Application.php
@@ -0,0 +1,84 @@
+<?php
+namespace PHPCtags;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Application extends \Symfony\Component\Console\Application
+{
+    private $commandName;
+
+    public function __construct(Command $command, $version = 'UNKNOWN')
+    {
+        parent::__construct($command->getName(), $version);
+
+        $this->add($command);
+        $this->commandName = $command->getName();
+
+        // Override the Application's definition so that it does not
+        // require a command name as first argument.
+        $this->getDefinition()->setArguments();
+    }
+
+    protected function getCommandName(InputInterface $input)
+    {
+        return $this->commandName;
+    }
+
+    public function add(Command $command)
+    {
+        // Fail if we already set up the single accessible command.
+        if ($this->commandName) {
+            throw new \OverflowException("You should not add more than one command to a PHPCtags\Application instance.");
+        }
+
+        return parent::add($command);
+    }
+
+    public function renderException($e, $output)
+    {
+        $formatter = $this->getHelperSet()->get('formatter');
+
+        do {
+            $exception = get_class($e);
+            $messages = $e->getMessage();
+            $messages = str_replace("\n", '', $messages);
+            $messages = str_replace("\r", '', $messages);
+
+            $formattedLine = $formatter->formatSection($exception, $messages, 'error');
+            $output->writeln($formattedLine);
+            $output->writeln("");
+
+            if (OutputInterface::VERBOSITY_VERBOSE <= $output->getVerbosity()) {
+                $output->writeln('<comment>Exception trace:</comment>');
+
+                // exception related properties
+                $trace = $e->getTrace();
+                array_unshift($trace, array(
+                    'function' => '',
+                    'file' => $e->getFile() != null ? $e->getFile() : 'n/a',
+                    'line' => $e->getLine() != null ? $e->getLine() : 'n/a',
+                    'args' => array(),
+                ));
+
+                for ($i = 0, $count = count($trace); $i < $count; $i++) {
+                    $class = isset($trace[$i]['class']) ? $trace[$i]['class'] : '';
+                    $type = isset($trace[$i]['type']) ? $trace[$i]['type'] : '';
+                    $function = $trace[$i]['function'];
+                    $file = isset($trace[$i]['file']) ? $trace[$i]['file'] : 'n/a';
+                    $line = isset($trace[$i]['line']) ? $trace[$i]['line'] : 'n/a';
+
+                    $output->writeln(sprintf(' %s%s%s() at <info>%s:%s</info>', $class, $type, $function, $file, $line));
+                }
+
+                $output->writeln("");
+            }
+        } while ($e = $e->getPrevious());
+
+        $formattedLine = $formatter->formatSection('Tip', 'Using "--help" to get more information.', 'comment');
+        $output->writeln($formattedLine);
+        $output->writeln("");
+    }
+}
---
+++ src/PHPCtags/Command/GenerateCtagsCommand.php
@@ -0,0 +1,39 @@
+<?php
+namespace PHPCtags\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GenerateCtagsCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('phpctags')->setDescription('The phpctags command');
+
+        $this->addArgument('files', InputArgument::REQUIRED | InputArgument::IS_ARRAY, 'Files/Folders to process');
+
+        $this->addOption('append', 'a', InputOption::VALUE_OPTIONAL, 'Append the tags to an existing tag file. <info>[yes|no]</info>', 'no');
+        $this->addOption('output', 'f|o', InputOption::VALUE_REQUIRED, 'Write tags to specified file. Value of "-" writes tags to stdout', 'tags');
+        $this->addOption('cache', 'C', InputOption::VALUE_OPTIONAL, 'Use a cache file to store tags for faster updates.', 'tags.cache');
+        $this->addOption('excmd', '', InputOption::VALUE_REQUIRED, 'Uses the specified type of EX command to locate tags. <info>[number|pattern|mix]</info>', 'mix');
+        $this->addOption('exclude', '', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Exclude files and directories matching pattern.');
+        $this->addOption('recursive', 'R', InputOption::VALUE_OPTIONAL, 'Recurse into directories supplied on command line. <info>[yes|no]</info>', 'no');
+        $this->addOption('fields', '', InputOption::VALUE_REQUIRED, 'Include selected extension fields <info>(flags: "[+|-]afmikKlnsStz").</info>', 'fks');
+        $this->addOption('format', '', InputOption::VALUE_REQUIRED, 'Force output of specified tag file format.', 2);
+        $this->addOption('sort', '', InputOption::VALUE_REQUIRED, 'Should tags be sorted (optionally ignoring case). <info>[yes|no|foldcase]</info>', 'yes');
+        $this->addOption('memory', '', InputOption::VALUE_REQUIRED, 'Set how many memories phpctags could use. <info>[-1|bytes|KMG]</info>', ini_get('memory_limit'));
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // $bar = $input->getOption('bar');
+        // $output->writeln('<info>PHPCtags</info>' . ($bar ? ' (barred)': ' (basic)'));
+
+        foreach ($input->getArgument('files') as $file) {
+            $output->writeln('File: ' . $file);
+        }
+    }
+}
