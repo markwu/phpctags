7222d8ee7e0cc75ae97bb833a59fdade19ed9686
Mark Wu <wuhsinyee@hotmail.com>
2013-12-09 02:31:42 +0800

    Some cleanup

--- src/PHPCtags/Application.php
+++ src/PHPCtags/Application.php
@@ -3,7 +3,6 @@ namespace PHPCtags;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Application extends \Symfony\Component\Console\Application
