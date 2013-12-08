9b0543e27080f90f5ad6445247973c50ccb37adc
Mark Wu <wuhsinyee@hotmail.com>
2013-12-09 02:27:51 +0800

    Add --debug option

--- src/PHPCtags/Command/GenerateCtagsCommand.php
+++ src/PHPCtags/Command/GenerateCtagsCommand.php
@@ -16,7 +16,7 @@ class GenerateCtagsCommand extends Command
         $this->addArgument('files', InputArgument::REQUIRED | InputArgument::IS_ARRAY, 'Files/Folders to process');
 
         $this->addOption('append', 'a', InputOption::VALUE_OPTIONAL, 'Append the tags to an existing tag file. <info>[yes|no]</info>', 'no');
-        $this->addOption('output', 'f|o', InputOption::VALUE_REQUIRED, 'Write tags to specified file. Value of "-" writes tags to stdout', 'tags');
+        $this->addOption('output', 'f|o', InputOption::VALUE_REQUIRED, 'Write tags to specified file. Value of <info>"-"</info> writes tags to stdout', 'tags');
         $this->addOption('cache', 'C', InputOption::VALUE_OPTIONAL, 'Use a cache file to store tags for faster updates.', 'tags.cache');
         $this->addOption('excmd', '', InputOption::VALUE_REQUIRED, 'Uses the specified type of EX command to locate tags. <info>[number|pattern|mix]</info>', 'mix');
         $this->addOption('exclude', '', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Exclude files and directories matching pattern.');
@@ -25,6 +25,7 @@ class GenerateCtagsCommand extends Command
         $this->addOption('format', '', InputOption::VALUE_REQUIRED, 'Force output of specified tag file format.', 2);
         $this->addOption('sort', '', InputOption::VALUE_REQUIRED, 'Should tags be sorted (optionally ignoring case). <info>[yes|no|foldcase]</info>', 'yes');
         $this->addOption('memory', '', InputOption::VALUE_REQUIRED, 'Set how many memories phpctags could use. <info>[-1|bytes|KMG]</info>', ini_get('memory_limit'));
+        $this->addOption('debug', '', InputOption::VALUE_NONE, "PHPCtags only, respect PHP's error level configuration.");
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
